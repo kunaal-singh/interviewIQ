18a23bfb02b972117d04e91be5321a9848e04d1c
Kunal Singh <kunaalsingh1637@gmail.com>
2026-09-20 13:49:06 +0530

    Change ServerUrl to production endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
 
-export const ServerUrl = "http://localhost:8000"
+export const ServerUrl = "https://interviewiq-s25u.onrender.com"
 
 function App() {
 
